b39c6dd7db0402166df2fb2297c9a42a7a2dc6f9
Chris Rodriguez <cerdevelopers@gmail.com>
2018-08-04 21:19:22 -0500

    test: create tellJoke method to test if joke would be passed from java library and called the method in MainActivity

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import christopher.builditbigger.Joke;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joke joke = new Joke();
+
+        Toast.makeText(this, joke.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
--- javaJokeLib/src/main/java/christopher/builditbigger/Joke.java
+++ javaJokeLib/src/main/java/christopher/builditbigger/Joke.java
@@ -1,4 +1,8 @@
 package christopher.builditbigger;
 
 public class Joke {
+
+    public String tellJoke() {
+        return "What do you call cheese that is not yours? Nacho cheese!";
+    }
 }
